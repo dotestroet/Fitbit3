3b2942b857d23eb1f5427bd7b121f52e84a5c8f6
Roedie86 <125372026+Roedie86@users.noreply.github.com>
2025-03-18 19:42:35 +0100

    updated database_queries with additional code to connect db to dashbaord

--- scripts/database_queries.py
+++ scripts/database_queries.py
@@ -1,11 +1,14 @@
 import sqlite3
 import pandas as pd
+import os
 
-DB_PATH = "data/fitbit_database.db"
-MODIFIED_DB_PATH = "data/fitbit_database_modified.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "..", "data", "fitbit_database.db")
+MODIFIED_DB_PATH = os.path.join(BASE_DIR, "..", "data", "fitbit_database_modified.db")
 
-def get_table_names():
-    conn = sqlite3.connect(DB_PATH)
+def get_table_names(use_modified=False):  
+    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH  
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = cursor.fetchall()
@@ -13,8 +16,9 @@ def get_table_names():
     
     return [table[0] for table in tables]
 
-def get_column_names(table_name):
-    conn = sqlite3.connect(DB_PATH)
+def get_column_names(table_name, use_modified=False):  
+    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH 
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute(f"PRAGMA table_info({table_name})")
     columns = cursor.fetchall()
@@ -22,18 +26,16 @@ def get_column_names(table_name):
     
     return [col[1] for col in columns]
 
-def fetch_table_data(table_name, use_modified=False):
-    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH
-    
+def fetch_table_data(table_name, use_modified=False):  
+    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH  
     conn = sqlite3.connect(db_path)
     df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
     conn.close()
     
     return df
 
-def save_table_data(df, table_name, use_modified=False):
-    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH
-    
+def save_table_data(df, table_name, use_modified=False): 
+    db_path = MODIFIED_DB_PATH if use_modified else DB_PATH 
     conn = sqlite3.connect(db_path)
     df.to_sql(table_name, conn, if_exists='replace', index=False) 
     conn.close()
